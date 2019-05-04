47399d53474622ae672a04408287aa5691460533
Adam S <leavinit@gmail.com>
2019-05-03 22:20:03 -0500

    added logic to strip api response of the relevant data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,62 @@
-var apiKey = "SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7";
+const API_KEY = "SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7";
 const IMG_LIMIT = 10;
 var query;
-
-var apiURL = "https://api.giphy.com/v1/gifs/search?" +
-"api_key=SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7&q="+query+
-"&limit="+IMG_LIMIT+"&offset=0&rating=R&lang=en";
-
-
 var topics = [
     "Inceptionism","Neural Network","Fractal","Geodesic","Deepdream","Alex Gray",
     "Visual Illusion","Computer Art"
 ];
-console.log(topics);
+
+
+
 function displayButtons(items){
     for (var i in items){
         var topic = items[i];
-        var msg = $("<button data-item="+topic+" class=topicBtn>").text(topic);
+        var msg = $("<button data-item="+topic+" class=topicBtn>    ").text(topic);
         $("#buttonsDiv").prepend(msg);
     }
 
 }
 
+//returns an array of objects that store the data we need to display at a later point
+function fetchData(searchTerm){
+    
+    var apiURL = "https://api.giphy.com/v1/gifs/search?" +
+    "api_key="+API_KEY+"&q="+searchTerm+
+    "&limit="+IMG_LIMIT+"&offset=0&rating=R&lang=en";
+    
+    var dataObj;
+    var dataContainer = [];
+    $.ajax({
+        url: apiURL,
+        method: "GET"
+    }).then(function(response) {
 
-    displayButtons(topics);
+        response.data.forEach(function(item){
+            dataObj = {
+                "term"  : searchTerm,
+                "images" : {
+                    "still" : item.images.fixed_height_still.url,
+                    "gif"   : item.images.fixed_height.webp
+                },
+                "rating"   : item.rating
+                // "tags"      : item
+            }
+            dataContainer.push(dataObj);
+        });
+
+    });
+    return dataContainer;    
+}
+
+//Display Functions
+
+//Definitely want to display stills and data first then load the animated pics later (or onclick)
 
 
+
+
+
+
+// displayButtons(topics);
+var testRun=fetchData('deepdream');   
+console.log(testRun);
