473b5b025d5f0d0611394690747a228c9e6b56ad
Adam S <leavinit@gmail.com>
2019-05-04 00:39:09 -0500

    added function to display stills and gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,56 +7,70 @@ var topics = [
 ];
 
 
-
 function displayButtons(items){
     for (var i in items){
         var topic = items[i];
-        var msg = $("<button data-item="+topic+" class=topicBtn>    ").text(topic);
+        var msg = $("<button data-item="+topic+" class=topicBtn>").text(topic);
         $("#buttonsDiv").prepend(msg);
     }
 
 }
 
-//returns an array of objects that store the data we need to display at a later point
 function fetchData(searchTerm){
     
     var apiURL = "https://api.giphy.com/v1/gifs/search?" +
     "api_key="+API_KEY+"&q="+searchTerm+
     "&limit="+IMG_LIMIT+"&offset=0&rating=R&lang=en";
     
-    var dataObj;
-    var dataContainer = [];
+
     $.ajax({
         url: apiURL,
         method: "GET"
     }).then(function(response) {
-
+        
         response.data.forEach(function(item){
-            dataObj = {
-                "term"  : searchTerm,
-                "images" : {
-                    "still" : item.images.fixed_height_still.url,
-                    "gif"   : item.images.fixed_height.webp
-                },
-                "rating"   : item.rating
-                // "tags"      : item
-            }
-            dataContainer.push(dataObj);
+             
+            var dataObj = {
+                "term"      : searchTerm,
+                "title"     : item.title,
+                "images"    : {
+                        "still" : item.images.fixed_height_still.url,
+                        "gif"   : item.images.fixed_height.webp
+                    },
+                "rating"    : item.rating
+            };
+            // console.log(dataObj);
+            displayStills(dataObj);
         });
-
+            
     });
-    return dataContainer;    
+
+    
 }
 
 //Display Functions
 
 //Definitely want to display stills and data first then load the animated pics later (or onclick)
+function displayStills(container){
+    //fill in
+    // console.log(container.images.still);
+    var el =$("<img>");
+    el.attr("src",container.images.still);
+    // console.log(el.attr("src"));
+    $("#stillDiv").append(el);
+    
+    var elgif =$("<img>");
+    elgif.attr("src",container.images.gif);
+    $("#gifDiv").append(elgif);
+
+    
+
+}
 
 
 
+displayButtons(topics);
 
+fetchData('deepdream');
+// console.log (stillDivs)
 
-
-// displayButtons(topics);
-var testRun=fetchData('deepdream');   
-console.log(testRun);
