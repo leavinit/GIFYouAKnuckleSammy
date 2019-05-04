150b0211494dc22ce42d08610703bc336c32a6e8
Adam S <leavinit@gmail.com>
2019-05-03 20:46:33 -0500

    added topic list and js to display each topic as a button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,27 @@
-apiKey = "SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7";
-topics = {};
+var apiKey = "SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7";
+const IMG_LIMIT = 10;
+var query;
+
+var apiURL = "https://api.giphy.com/v1/gifs/search?" +
+"api_key=SgJpkOUY4f5rrojGpZOiKEf4i1clPMC7&q="+query+
+"&limit="+IMG_LIMIT+"&offset=0&rating=R&lang=en";
+
+
+var topics = [
+    "Inceptionism","Neural Network","Fractal","Geodesic","Deepdream","Alex Gray",
+    "Visual Illusion","Computer Art"
+];
+console.log(topics);
+function displayButtons(items){
+    for (var i in items){
+        var topic = items[i];
+        var msg = $("<button data-item="+topic+" class=topicBtn>").text(topic);
+        $("#buttonsDiv").prepend(msg);
+    }
+
+}
+
+
+    displayButtons(topics);
+
+
